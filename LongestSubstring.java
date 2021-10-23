ff49f799fe58818fa807db332111e3f229b5e8d2
coder27PB <93054888+coder27PB@users.noreply.github.com>
2021-10-24 00:58:00 +0530

    Create LongestSubstring.java

---
+++ LongestSubstring.java
@@ -0,0 +1,37 @@
+package com.example.priya.model.enums;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class LongestSubstring {
+
+    /**
+     * static
+     * atic
+     * codingisawesome
+     * ngisawe
+     **/
+
+    public static void main(String[] args) {
+        String str = "codingisawesome";
+        LinkedHashSet<Character> tracker = new LinkedHashSet<>();
+        int max = 0;
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+
+            for (int j = i + 1; j < str.length(); j++) {
+                if (!tracker.contains(str.charAt(i))) {
+                    tracker.add(str.charAt(i));
+                } else {
+                    if (tracker.size() > max) {
+                        max = tracker.size();
+                        answer = tracker.toString();
+
+                    }
+
+                }
+            }
+        }
+    }
+
+}
